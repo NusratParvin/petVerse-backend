fd8ab18f7354bbe11e651f9fff73789f16eeef4c
NusratParvin <nusrat.official@gmail.com>
2026-04-13 02:28:33 +0400

    vercel.json

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,3 @@
-// import express, { Application, Request, Response } from 'express';
-// import cors from 'cors';
-// import cookieParser from 'cookie-parser';
-
-// import router from './app/routes';
-// import globalErrorHandler from './app/middlewares/globalErrorHandler';
-// import notFound from './app/middlewares/notFound';
-
-// const app: Application = express();
-// app.use(
-//   cors({
-//     origin: ['http://localhost:3000', 'https://fishcove-client.vercel.app'],
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-//   }),
-// );
-// app.use(express.json());
-// // app.use(cors());
-// app.use(cookieParser());
-
-// // app.use(
-// //   cors({
-// //     origin: 'https://fishcove-client.vercel.app',
-// //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-// //     credentials: true,
-// //   }),
-// // );
-// // app.options('*', cors());
-// app.use('/api/v1', router);
-
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Welcome to fishCove!');
-// });
-
-// app.use(globalErrorHandler);
-
-// app.use(notFound);
-// export default app;
-
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -46,22 +7,28 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
-
 app.use(
   cors({
     origin: ['http://localhost:3000', 'https://fishcove-client.vercel.app'],
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // Array format preferred
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    exposedHeaders: ['Set-Cookie'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
 
+app.options('*', cors());
+
 app.use(express.json());
+// app.use(cors());
 app.use(cookieParser());
 
-// app.options('*', cors());
-
+// app.use(
+//   cors({
+//     origin: 'https://fishcove-client.vercel.app',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//   }),
+// );
 app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
@@ -69,6 +36,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
-app.use(notFound);
 
+app.use(notFound);
 export default app;
